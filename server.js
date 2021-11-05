5ecfb2c89010e7964af0aa61750ea59a6249efbf
PurviMec <princy.mecwan94@gmail.com>
2021-11-05 18:39:33 -0400

    create get post createnewnote functions

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
+const fs = require('fs');
+const path = require('path');
+
 const express = require('express');
 const {notes} = require('./Develop/db/db.json');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 
 function filterByQuery(query, notesArray){
     let filteredResults = notesArray;
@@ -15,6 +22,27 @@ function filterByQuery(query, notesArray){
     return filteredResults;
 }
 
+function createNewNote (body, notesArray){
+    const note = body;
+    notesArray.push(note);
+    fs.writeFileSync(
+        path.join(__dirname, './Develop/db/db.json'),
+        JSON.stringify({ notes: notesArray }, null, 2)
+    );
+
+    return note;
+}
+
+function validateData(note) {
+    if (!note.title || typeof note.title !== 'string'){
+        return false;
+    }
+    if (!note.text || typeof note.text !== 'string'){
+        return false;
+    }
+    return true;
+}
+
 app.get('/api/notes', (req, res) => {
     let results = notes;
     if (req.query){
@@ -22,7 +50,15 @@ app.get('/api/notes', (req, res) => {
     }
     res.json(results);
 });
-  
+
+app.post('/api/notes', (req, res) => {
+    if (!validateData(req.body)){
+        res.status(400).send('Please formate notes properly!');
+    }else{
+        const note = createNewNote(req.body, notes);
+        res.json(note);
+    }
+});
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
