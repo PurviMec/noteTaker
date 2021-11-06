5619c4a641a5be361d68ee15da2b9ae70a5ee9a0
PurviMec <princy.mecwan94@gmail.com>
2021-11-06 10:46:38 -0400

    change file structure

--- server.js
+++ server.js
@@ -7,8 +7,10 @@ const {notes} = require('./Develop/db/db.json');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+app.use(express.static('Develop'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 
 
 function filterByQuery(query, notesArray){
@@ -59,6 +61,15 @@ app.post('/api/notes', (req, res) => {
         res.json(note);
     }
 });
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './Develop/public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './Develop/public/notes.html'));
+});
+
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
